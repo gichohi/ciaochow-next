9851f5d118fc7cedfa71ada39ea417ec364ed33f
Gichohi <gichohi@PC109-C02YT33ELVDL.local>
2024-03-13 09:52:34 +0300

    added login tests

--- src/app/ui/login/__tests__/index.test.tsx
+++ src/app/ui/login/__tests__/index.test.tsx
@@ -1,4 +1,4 @@
-import { act, fireEvent, render } from "@testing-library/react";
+import { act, fireEvent, render, waitFor } from "@testing-library/react";
 import { Login } from "..";
 import React from "react";
 
@@ -50,7 +50,46 @@ describe('<LoginForm/>', () => {
         expect(passwordInput).toBeInTheDocument();
         expect(passwordInput.value).toBe(PASSWORD);
 
-        const loginButton = loginForm.getByTestId("btnLogin") as HTMLInputElement;
+        const loginButton = loginForm.getByTestId("login") as HTMLInputElement;
+        act(() => {
+            fireEvent.click(loginButton);
+        });
+
+        await waitFor(() => {
+            expect(loginButton.disabled).toEqual(false);
+        });
+
+    });
+
+    it('disables submit button if inputs are invalid', async () => {
+        const loginForm = render(<Login />);
+
+        const emailInput = loginForm.getByTestId("email") as HTMLInputElement;
+        emailInput.focus();
+        act(() => {
+            fireEvent.change(emailInput, { target: { value: EMAIL } });
+        });
+    
+        const passwordInput = loginForm.getByTestId("password") as HTMLInputElement;
+        passwordInput.focus();
+        act(() => {
+            fireEvent.change(passwordInput, { target: { value: "demo" } });
+        });
+    
+        expect(emailInput).toBeInTheDocument();
+        expect(emailInput.value).toBe(EMAIL);
+    
+        expect(passwordInput).toBeInTheDocument();
+        expect(passwordInput.value).toBe("demo");
+
+        const loginButton = loginForm.getByTestId("login") as HTMLInputElement;
+        act(() => {
+            fireEvent.click(loginButton);
+        });
+
+        await waitFor(() => {
+            expect(loginButton.disabled).toEqual(true);
+        });
 
     });
 
--- src/app/ui/login/titleview.tsx
+++ src/app/ui/login/titleview.tsx
@@ -9,6 +9,8 @@ export const TitleView = () => {
                 src="../../ellipse.svg"
                 alt="Background Image"
                 className="w-full h-2/4"
+                width={100}
+                height={100}
             />
             <div className="absolute top-60 left-5 flex items-start justify-start">
                 <Title />
@@ -17,6 +19,8 @@ export const TitleView = () => {
                     <Image
                         src="../../login.svg"
                         alt="login image"
+                        width={100}
+                        height={100}
                     />
             </div>
 
